cc2e78de11d022c8c8fab8ba50aa4b8cc21b2a86
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-09-13 17:59:33 +0545

    assignment 1 complete

---
+++ Rajesh Pudasaini - Student/CSharp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp1
+{
+    class Program
+    {
+        static void MainProgram(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
