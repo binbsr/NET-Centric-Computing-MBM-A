7b5c8d8f02107912c2b3e73a931abc409139d286
Sandhya Neupane <iamsandhyaneupane@gmail.com>
2020-09-13 12:22:09 +0545

    CSharpBasics

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,39 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main1()
+//     {
+//         // 1. Value types
+//         // 1.1 Numbers
+//         // 1.1.1 Integers
+//         byte a = 3;
+//         short b = 345;
+//         int c = 123456;
+//         long d = 33487472923556;
+
+//         // 1.1.2 Floating numbers
+//         float e = 1234.5f;
+//         double f = 3456.8;
+//         decimal g = 121486.3486965749m;
+
+
+//         // 1.2 Characters
+//         char h = 'a';
+
+//         // 1.3 Boolean
+//         bool i =  true;
+
+//         // 2. Reference type
+//         string j = "abscdjf"; 
+//         object k = 324.566;
+        
+
+
+
+//         // Console.Write("Enetr your name: ");
+//         // string x = Console.ReadLine();
+//         // Console.WriteLine("Hi, " + x);
+//     }
+    
+// }
