ad49bab0719ab0ff39b1504faf2070b30c8628b9
Anusha Tandukar <anushatandukar67@gmail.com>
2020-09-13 13:49:24 +0545

    Added new file

---
+++ Anusha Tandukar - Student/CSharpBasicss/Constructor.cs
@@ -0,0 +1,34 @@
+namespace AllAboutClasses
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public readonly double AREA = 4323.76;
+        //Default constructor
+        public Country()
+        {
+
+        }
+        // Parameterized constructor
+        public Country(string name)
+        {
+            Name = name;
+        }
+        public Country(string name, int population, double area)
+        {
+            Name = name;
+            Population = population;
+            AREA = area;
+        }
+    }
+    public class Demo
+    {
+        void DoSomething()
+        {
+            Country country1 = new Country();
+            Country country2 = new Country("Nepal");
+
+        }
+    }
+}
