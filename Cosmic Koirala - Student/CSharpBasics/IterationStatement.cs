5f0ac5e0a5bdcd6d936ddc0ba552737ebc6d2686
Cosmic Koirala <koiralacosmic@gmail.com>
2020-09-13 12:05:34 +0545

    Added something

---
+++ Cosmic Koirala - Student/CSharpBasics/IterationStatement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class IterationStatement
+{
+    static void Main12345()
+    {
+        IterationStatement iterationStatement = new IterationStatement();
+        iterationStatement.LearnWhileloop();
+
+    }
+
+
+   void LearnForloop()
+    {
+        for(int i = 0; i<10; i++)
+        Console.WriteLine("I am Cosmic");
+    }
+
+     void LearnWhileloop()
+    {
+        char confirm = 'Y';
+        while(confirm == 'Y')
+        {
+        Console.WriteLine("I am Cosmic");
+        Console.WriteLine("Wanna continue?");
+        confirm = Console.ReadKey().KeyChar;
+        }
+    }
+
+    //  int[] numbers = {1,2,3,4,5};
+
+    //  foreach (int x in numbers)
+    //  {
+    //      if(x%2==0)
+    //      {
+    //          Console.WriteLine($"{x} is even.")
+    //      }
+    //      else
+    //      {
+    //          Console.WriteLine($"{x} is odd.")
+    //      }
+    //  }
+
+}
