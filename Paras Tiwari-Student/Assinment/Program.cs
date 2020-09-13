327d589ffa0f551afc778f8faf35d8ddb71bbc87
Paras Tiwari <parastiware@gmail.com>
2020-09-13 16:58:58 +0545

    Assignment completed

--- Paras Tiwari-Student/Assinment/Program.cs
+++ Paras Tiwari-Student/Assinment/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace Assinment
+namespace Assignment
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Method method1 = new Method();
+    //Sum of number
             Console.WriteLine("Enter First Number");
             int num1= int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Second Number");
@@ -20,6 +22,21 @@ namespace Assinment
             {
                 Console.WriteLine($"Sum of {num1} and {num2} is not greater than 500 so result is {result}");
             }
+    //point caluator
+            Console.WriteLine("Enter the  Number of wins: ");
+            int wins= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the  Number of Draws: ");
+            int draws= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the  Number of losses: ");
+            int losses= int.Parse(Console.ReadLine());
+            int points = method1.PointsCalulator(wins,draws,losses); // calling second Method
+            Console.WriteLine($"Your team played {wins+draws+losses} matches and earned {points} points.");
+    //Initails generator
+             Console.WriteLine("Enter your name: ");
+             string name = Console.ReadLine();
+             string initials = method1.GetInitials(name);
+             Console.WriteLine($"Your initials are {initials}");
+
         }
     }
 }
